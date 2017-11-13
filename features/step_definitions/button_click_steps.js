eaa5890370eb3f04aba63515a463a8c0229f4340
ghatzikotelis <ghatzikotelis@singlestoneconsulting.com>
2017-11-13 17:29:57 -0500

    Added callback to step defs. added debug msgs

--- features/step_definitions/button_click_steps.js
+++ features/step_definitions/button_click_steps.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const defineSupportCode = require('cucumber').defineSupportCode;
 const app2  = require('../support/hooks')
 const world = require('../support/world').World;
-var AppPage2 =  require('../step_definitions/support/pages/js_training');
+const AppPage2 =  require('../step_definitions/support/pages/js_training');
 
 defineSupportCode(({Given, When, Then, Before, World}) => {
 
@@ -26,16 +26,18 @@ defineSupportCode(({Given, When, Then, Before, World}) => {
   Given(/^I am on the test site$/,
       function (callback) {
           app3.navigateTo()
+          callback();
       });
 
 
   When(/^I click the submit button$/,
       function (callback) {
           app3.clickSubmit()
+          callback();
       });
 
   Then(/^I should see the div change color$/,
-    function(callback) {
+    function (callback) {
         app3.getClickResults(ele)
         expect(ele.getAttribute('class')).to.contain('qa-red')
     })
--- features/step_definitions/support/pages/js_training.js
+++ features/step_definitions/support/pages/js_training.js
@@ -10,21 +10,22 @@ class AppPage {
 
   navigateTo() {
     browser.waitForAngularEnabled(false);
-    console.log('getting the url')
+    console.log('getting the url');
     return browser.get('/');
   }
 
   clickSubmit() {
-    console.log('clicking button')
+    console.log('clicking button');
     return this.submit.click();
   }
 
   getClickResults() {
     browser.takeScreenshot().then(function(png){
         writeScreenShot.writeScreenShot(png, 'yo0000000000.png');
-        return element(by.className('qa-main-div'));
 
-      })
+      });
+
+    return element(by.className('qa-main-div'));
 
   }
 
